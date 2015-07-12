252b86c7d7eb24e76149d1faf4c63b60c712482f
sebastian <sebastian.rincon13@gmail.com>
2015-07-12 16:58:49 -0500

    permisos u

--- exposicion/userprofiles/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .forms import*
-# Create your views here.
-def signup(request):
-	form = UserCreationEmailForm(request.POST or None)
-
-	if form.is_valid():
-		form.save()
-	return render(request, 'signup.html', {'form':form})
